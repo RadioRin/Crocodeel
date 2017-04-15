37922a3703e2df8501d40a112621382f963ed30c
Mxsol <maximsolonin@gmail.com>
2017-04-15 13:09:42 +0300

    nas

--- readdata.R
+++ readdata.R
@@ -1,7 +1,8 @@
 library(jsonlite)
 library(stringr)
 library(dplyr)
-j = readLines("~/Crocodeal/beers.jsonlines") %>% 
+j = readLines("~/Projects/Crocodeal/Crocodeal/beers.jsonlines") %>% 
   str_c(collapse = ",") %>%  
   (function(str) str_c("[", str, "]")) %>% 
   fromJSON(simplifyDataFrame = T)
+j<-na.omit(j)
