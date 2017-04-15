8a4c74644bc12d9ce3b4a5d507608e4ac1fafde5
Daria Maglevanaya <maglevanaya.dary@gmail.com>
2017-04-15 12:43:45 +0300

    read

---
+++ readdata.R
@@ -0,0 +1,7 @@
+library(jsonlite)
+library(stringr)
+library(dplyr)
+j = readLines("~/Crocodeal/beers.jsonlines") %>% 
+  str_c(collapse = ",") %>%  
+  (function(str) str_c("[", str, "]")) %>% 
+  fromJSON(simplifyDataFrame = T)
