8ac640e99cb98e6aab822d6df7f0233f339b85b4
Arina Makkoveeva <ammakkoveeva@edu.hse.ru>
2017-04-16 15:15:30 +0300

    added some bottoms

--- Beer/app.R
+++ Beer/app.R
@@ -1,50 +1,32 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
 #    http://shiny.rstudio.com/
 #
 
+library(jsonlite)
+library(stringr)
+library(dplyr)
+j = readLines("~/HELL/Crocodeal/beers.jsonlines") %>% 
+  str_c(collapse = ",") %>%  
+  (function(str) str_c("[", str, "]")) %>% 
+  fromJSON(simplifyDataFrame = T)
+j<-na.omit(j)
+
+
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  sliderInput(inputId = "num",
+              label = "Выберите крепость пива (в градусах)",
+              value = 6, min = 0, max = 15),
+  sliderInput(inputId = "num1",
+              label = "Выберите густоту пива",
+              value = 12, min = 0.5, max = 27),
+  checkboxGroupInput(inputId = "num2",
+                     label = "Выберите вкус пива",
+                     choices = c("Вишневое", "Ореховое", "Со вкусом дуба"))
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+server <- function(input, output) {}
 
-# Run the application 
+
 shinyApp(ui = ui, server = server)
-
