6a32880e0715ce02d27df44e29d9c956cfc7960d
Mxsol <maximsolonin@gmail.com>
2017-04-22 12:33:56 +0300

    Upd Sliders

--- Beer/app.R
+++ Beer/app.R
@@ -5,7 +5,7 @@
 library(jsonlite)
 library(stringr)
 library(dplyr)
-j = readLines("~/HELL/Crocodeal/beers.jsonlines") %>% 
+j = readLines("~/Projects/Crocodeal/Crocodeal/beers.jsonlines") %>% 
   str_c(collapse = ",") %>%  
   (function(str) str_c("[", str, "]")) %>% 
   fromJSON(simplifyDataFrame = T)
@@ -17,10 +17,10 @@ library(shiny)
 ui <- fluidPage(
   sliderInput(inputId = "num",
               label = "Выберите крепость пива (в градусах)",
-              value = 6, min = 0, max = 15),
+              value = c(4,7), min = 0, max = 15),
   sliderInput(inputId = "num1",
               label = "Выберите густоту пива",
-              value = 12, min = 0.5, max = 27),
+              value = c(12,21), min = 0.5, max = 27),
   checkboxGroupInput(inputId = "num2",
                      label = "Выберите вкус пива",
                      choices = c("Вишневое", "Ореховое", "Со вкусом дуба"))
