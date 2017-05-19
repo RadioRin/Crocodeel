351a3caa5cba2755addeb0e9c169e88f828d2f4f
Arina Makkoveeva <ammakkoveeva@edu.hse.ru>
2017-05-19 20:59:54 +0300

    Внесла изменения

--- Beer/app.R
+++ Beer/app.R
@@ -1,32 +1,62 @@
-#
-#    http://shiny.rstudio.com/
-#
-
-library(jsonlite)
-library(stringr)
-library(dplyr)
-j = readLines("~/HELL/Crocodeal/beers.jsonlines") %>% 
-  str_c(collapse = ",") %>%  
-  (function(str) str_c("[", str, "]")) %>% 
-  fromJSON(simplifyDataFrame = T)
-j<-na.omit(j)
-
-
+## app.R ##
+library(shinydashboard)
 library(shiny)
 
-ui <- fluidPage(
-  sliderInput(inputId = "num",
-              label = "Выберите крепость пива (в градусах)",
-              value = 6, min = 0, max = 15),
-  sliderInput(inputId = "num1",
-              label = "Выберите густоту пива",
-              value = 12, min = 0.5, max = 27),
-  checkboxGroupInput(inputId = "num2",
-                     label = "Выберите вкус пива",
-                     choices = c("Вишневое", "Ореховое", "Со вкусом дуба"))
+## Сайт, где мы брали информацию и туториалы
+# https://rstudio.github.io/shinydashboard/structure.html
+# http://deanattali.com/blog/building-shiny-apps-tutorial/
+
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Бухнем?"),
+  ## Sidebar content
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Парамтры напитка", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Оценка различных видов пива", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+  ## Body content
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                column(width = 7,
+                       box(
+                         title = "Определите крепость желаемого напитка",
+                         width = 0.25,
+                         sliderInput("slider", "градусы:", 0.25, 14.75, 6),
+                         "Как известно, все алкогольные напитки различаются по градусам. Если для вас важна крепость пива, вы можете выбрать её двигая слайдер, если же этот параметр для вас не важен - укажите значение равное 1"
+                       ),
+                       box(
+                         title = "Определите оттенок желаемого напитка",
+                         width = NULL,
+                         checkboxGroupInput("color", NULL,
+                                     c("Светлое", "Тёмное", "Нефильтрованное")),
+                         "Если вы желаете, чтобы вам предложили конкретное из трех видов пива, выберите вас интересующее; если для вас цвет не важет - не выбирайте ничего"
+                       )
+                )
+              )
+      ),
+      
+      
+      # Second tab content
+      tabItem(tabName = "widgets",
+              h2("Widgets tab content")
+      )
+    )
+  )
 )
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
 
-
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
