5c1581987c562f389272d4ef5e9172acd6fab4cf
Daria Maglevanaya <maglevanaya.dary@gmail.com>
2017-05-22 19:18:22 +0300

    downloading members

--- readdata.R
+++ readdata.R
@@ -5,3 +5,15 @@ j = readLines("~/Crocodeal/beers.jsonlines") %>%
   str_c(collapse = ",") %>%  
   (function(str) str_c("[", str, "]")) %>% 
   fromJSON(simplifyDataFrame = T)
+
+bc = readLines("~/Crocodeal/beer_comments.jsonlines") %>% 
+  str_c(collapse = ",") %>%  
+  (function(str) str_c("[", str, "]")) %>% 
+  fromJSON(simplifyDataFrame = T)
+
+memb = readLines("~/Crocodeal/members.jsonlines") %>% 
+  str_c(collapse = ",") %>%  
+  (function(str) str_c("[", str, "]")) %>% 
+  fromJSON(simplifyDataFrame = T)
+
+
